2ea478cb52b2ab8edd4036157770e77d86feba4b
Angela <angelalamluu@gmail.com>
2021-11-07 01:06:37 -0800

    added log(mn) solution for search 2d matrix

--- 74_search_2D_matrix.js
+++ 74_search_2D_matrix.js
@@ -5,14 +5,36 @@
 //Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 //Output: true
 
-
 var searchMatrix = function(matrix, target) {
-    for (let i = 0; i < matrix.length; i++){
-        for(const num of matrix[i]){
-            if (num === target){
-                return true
-            }
+    let m = matrix.length
+    let n = matrix[0].length
+    let start = 0
+    let end = n*m-1
+    while (start <= end){
+        let mid = Math.floor((start+end)/2)
+        let row = Math.floor(mid/n)
+        let col = Math.floor(mid % n)
+        if (target === matrix[row][col]) {
+            return true
+        }
+        if (target > matrix[row][col]){
+            start = mid + 1
+        }
+        else{
+            end = mid - 1
         }
     }
     return false
 };
+
+
+// var searchMatrix = function(matrix, target) {
+//     for (let i = 0; i < matrix.length; i++){
+//         for(const num of matrix[i]){
+//             if (num === target){
+//                 return true
+//             }
+//         }
+//     }
+//     return false
+// };
